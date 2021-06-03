398a89e58db102fb9898709a09d9db9c400c4a17
Artyom Savich <artyom.savich92@gmail.com>
2021-06-03 17:28:56 +0300

    Delete any genre

--- src/com/solvd/savich/music/artist/Genre.java
+++ src/com/solvd/savich/music/artist/Genre.java
@@ -1,6 +1,6 @@
 package com.solvd.savich.music.artist;
 
 public enum Genre { //Create Enum Class
-    FOLK, SACRED, ACADEMIC, POPULAR, HIP_HOP, ROCK, JAZ, ELECTRONIC_MUSIC;
+    POPULAR, HIP_HOP, ROCK, ELECTRONIC_MUSIC;
 
 }
