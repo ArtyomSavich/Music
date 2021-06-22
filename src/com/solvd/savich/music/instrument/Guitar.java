168e27c456a6b0258d090ce2958746622a360aaf
Artyom Savich <artyom.savich92@gmail.com>
2021-06-22 18:40:11 +0300

    Add override toString

--- src/com/solvd/savich/music/instrument/Guitar.java
+++ src/com/solvd/savich/music/instrument/Guitar.java
@@ -19,6 +19,11 @@ public class Guitar extends Instrument implements Tunable{ //Override method tun
     }
 
     @Override
+    public String toString() {
+        return getNameOfInstrument() + " with number of strings "+ getNumbersOfString();
+    }
+
+    @Override
     public void tune() {
         System.out.println("Tune guitar");
     }
