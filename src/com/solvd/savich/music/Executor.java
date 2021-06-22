d872415c8301e0fe14fc6a22cf7f055ab4316fed
Artyom Savich <artyom.savich92@gmail.com>
2021-06-22 18:36:30 +0300

    Fix some bugs, add new exseptions

--- src/com/solvd/savich/music/Executor.java
+++ src/com/solvd/savich/music/Executor.java
@@ -1,14 +1,19 @@
 package com.solvd.savich.music;
 
 import com.solvd.savich.music.menu.Menu;
-
+import exceptions.MyExceptions;
 
 
 public class Executor {
 
     public static void main(String[] args) {
         Menu menu = new Menu();
-        menu.showMenu();
+        try {
+            menu.showMenu();
+        } catch (MyExceptions myExceptions) {
+            myExceptions.printStackTrace();
+        }
+
     }
 
 }
