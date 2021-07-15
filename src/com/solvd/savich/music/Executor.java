924934e2124b17628b71e26b4b9bfc76b72dda62
Artyom Savich <artyom.savich92@gmail.com>
2021-07-15 16:55:22 +0300

    Tested properties file

--- src/com/solvd/savich/music/Executor.java
+++ src/com/solvd/savich/music/Executor.java
@@ -8,12 +8,12 @@ import com.solvd.savich.music.properties.Util;
 public class Executor {
 
     public static void main(String[] args) {
-        /*Menu menu = new Menu();
+        Menu menu = new Menu();
         try {
             menu.showMenu();
         } catch (MyExceptions myExceptions) {
             myExceptions.printStackTrace();
-        }*/
+        }
         String test = Util.getPropertiesValue("test");
         String test1 = Util.getPropertiesValue("GGF");
         System.out.println(test);
