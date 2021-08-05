90fdf7b7fb60ec2425a1f224a2958c8aa1ab2fc6
Artyom Savich <artyom.savich92@gmail.com>
2021-08-05 18:08:57 +0300

    Add new methods

--- src/main/java/com/solvd/savich/music/Executor.java
+++ src/main/java/com/solvd/savich/music/Executor.java
@@ -1,5 +1,6 @@
 package com.solvd.savich.music;
 
+import com.solvd.savich.database.Connector;
 import com.solvd.savich.music.artist.Artist;
 import com.solvd.savich.music.artist.Singer;
 import com.solvd.savich.music.exceptions.MyExceptions;
@@ -8,6 +9,9 @@ import com.solvd.savich.music.properties.Util;
 import com.solvd.savich.utils.JsonExec;
 import org.apache.log4j.Logger;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 
@@ -18,21 +22,41 @@ public class Executor {
 
     public static void main(String[] args) {
 
-    /*    Menu menu = new Menu();
-        try {
+
+       /* try {
             menu.showMenu();
         } catch (MyExceptions myExceptions) {
             myExceptions.printStackTrace();
+        }*/
+      //  Menu menu = new Menu();
+        Connector connector = new Connector();
+     //   LOGGER.info(menu.getAllTracks(connector));
+       // Menu menu1 = new Menu();
+       // LOGGER.info(menu1.getBandById(connector, 2));
+        Menu menu2 = new Menu();
+        LOGGER.info(menu2.getSingerById(connector, 1));
+       // Menu menu3 = new Menu();
+       // LOGGER.info(menu3.getGenreById(connector, 1));
+        Menu menu4 = new Menu();
+        LOGGER.info(menu4.getTracksById(connector,1));
+        Menu menu5 = new Menu();
+        LOGGER.info(menu5.getAllAlbums(connector));
+      /*  String query = "Select * FROM track";
+        Connector connector = new Connector();
+        connector.getConnect();
+        try (PreparedStatement pst = connector.getConnect().prepareStatement(query)){
+            ResultSet resultSet = pst.executeQuery();
+            while (resultSet.next()){
+                LOGGER.info(resultSet.getString("track_name"));
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-        String test = Util.getPropertiesValue("PASSWORD");
+*/
 
-        LOGGER.info(test);
 
-        Util.setPropertiesValueWithResources("PASSWORD_1", "SOLVD");
-        Util.printAll();*/
-
-        Menu menu = new Menu();
-        JsonExec jsonAction = new JsonExec();
+        /*JsonExec jsonAction = new JsonExec();
         String jsonStr = jsonAction.convertJavaToJsonString(menu.getSingerFromBase());
         String jsonStr1 = jsonAction.convertJavaToJsonString(Menu.initArtists());
         LOGGER.info(jsonStr);
@@ -45,7 +69,7 @@ public class Executor {
 
         List<Singer> artist1 = jsonAction.convertJsonStrToListSingerPOJO("singer.json");
         LOGGER.info("Name Artist from POJO: " + artist1.get(1).getName());
-
+*/
 
 
     }
